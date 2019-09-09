5dd0eb62b32356caf779e07addff8ea1af632047
kranewasd <kranehecaru@gmail.com>
2019-09-09 10:43:21 -0700

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.Vector;
 
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Utility
 {
@@ -17,111 +20,54 @@ public class Utility
 	
 		return info;
 	}
-	
-	public void saveFile(Vector<String> toateNumeleRelevante)
-	{
 
-		FileOutputStream outputStream;
-		for(String s : toateNumeleRelevante)
-		{
-			 
-		    try
-			{
-				outputStream = new FileOutputStream("temp.txt", true);
-				 byte[] strToBytes = s.getBytes();
-				    outputStream.write(strToBytes);
-				  
-				    outputStream.close();
-			} catch (FileNotFoundException e1)
-			{
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IOException e1)
-			{
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-	}
 	
-	public Vector<String> readFile()
+	public String calcululButonuluiDeShuffle(Vector<String> totiEleviiDupaNume)
 	{
-		Vector<String> result = new Vector<String>();
-		File file = new File("temp.txt"); 
-	    Scanner sc;
-		try
-		{
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) 
-			      result.add(sc.nextLine()); 
-		} catch (FileNotFoundException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-			  
-			    
-		return result;
+		int int_calcululPentruCazuri = (totiEleviiDupaNume.size() - 5);
+		String calcululPentruCazuri = new String().valueOf(int_calcululPentruCazuri);
+		
+		boolean result = int_calcululPentruCazuri > 5 ? true : false;
+		
+		if (result) { return "Au mai ramas: " + calcululPentruCazuri; } 
+		else { return "Acestia sunt toti elevii"; }
+		
+		
 	}
 	
+	public void DisplayInfo(JTextField txtNume, JLabel numeleElev, JTextField txtInitTata, JTextField txtPrenume, Vector<String> INFORMATII_DESPRE_ELEV_DIN_DB, JLabel lblNumeComplet,
+							JLabel lblAnulabsolvirii, JLabel lblFormaabsolvirii, JLabel lblCalificarea, JLabel lblDiploma, JPanel ExtremeSearch, JPanel InformatiiElev)
+	{
+		Handle_XMLSX xlsx = new Handle_XMLSX();
+		String numeleComplet = numeleElev.getText();
+		System.out.println(numeleComplet);
+		numeleComplet = numeleComplet.toUpperCase();
+		
+		INFORMATII_DESPRE_ELEV_DIN_DB = xlsx.checkStatusFromName(numeleComplet, false);
+		
+		lblNumeComplet.setText("Nume: " + INFORMATII_DESPRE_ELEV_DIN_DB.get(0));
+		lblAnulabsolvirii.setText("Anul absolvirii: " + INFORMATII_DESPRE_ELEV_DIN_DB.get(1).substring(0, INFORMATII_DESPRE_ELEV_DIN_DB.get(1).indexOf('.')));
+		lblFormaabsolvirii.setText("Forma absolvirii: " + INFORMATII_DESPRE_ELEV_DIN_DB.get(2));
+		lblCalificarea.setText("Calificare: " + INFORMATII_DESPRE_ELEV_DIN_DB.get(3));
+		
+		String diplomaAnswer = INFORMATII_DESPRE_ELEV_DIN_DB.get(4).toUpperCase();
+		
+		if ( diplomaAnswer.equals("DA") )
+		{
+			lblDiploma.setText("Elevul a ridicat diploma.");
+		}
+		else if ( diplomaAnswer.equals("NU") )
+		{
+			lblDiploma.setText("Elevul nu a ridicat diploma.");
+			
+		}
+		else
+		{
+			lblDiploma.setText("Nu se stie daca are sau nu diploma.");
+		}
+		
+		ExtremeSearch.setVisible(false);
+		InformatiiElev.setVisible(true);
+	}
 }
 
-/*
- * Handle_XMLSX xlsx = new Handle_XMLSX();
-				String numeleComplet = txtNumeelev.getText() + " " + txtinitialaTatalui.getText() + " " + txtPrenume.getText();
-				numeleComplet = numeleComplet.toUpperCase();
-				
-				Vector<String> info = xlsx.checkStatusFromName(numeleComplet, false);
-				for(String s : info)
-				{
-					System.out.println(s);
-				}
-				if (info.size() < 1)
-				{
-					JOptionPane.showMessageDialog(null, "Acest elev nu are informatiile trecute sau nu exista in baza de date.");
-					
-					int dialogButton = JOptionPane.YES_NO_OPTION;
-					int dialogResult = JOptionPane.showConfirmDialog(null, "Doriti sa cautati nume asemanatoare?", "Cautare extrema", dialogButton);
-					if(dialogResult == 0) 
-					{
-						
-						String numeExtreme = JOptionPane.showInputDialog(null, "Nume: ");
-						numeExtreme = numeExtreme.toUpperCase();
-						Vector<String> toateNumeleRelevante = new Utility().extremSearch(numeExtreme);
-						new Utility().saveFile(toateNumeleRelevante);
-						
-						for(String s : new Utility().readFile())
-						{
-							System.out.println(s);
-						}
-						
-						
-					} 
-					else 
-					{
-					
-					} 				
-				}
-				else
-				{
-					lblNumeComplet.setText("Nume: " +info.get(0));
-					lblAnulabsolvirii.setText("Anul absolvirii: " + info.get(1).substring(0, info.get(1).indexOf('.')));
-					lblFormaabsolvirii.setText("Forma absolvirii: " + info.get(2));
-					lblCalificareaprofesionala.setText("Calificare: " + info.get(3));
-					
-					String diplomaAnswer = info.get(4).toUpperCase();
-					
-					if ( diplomaAnswer.equals("DA") )
-					{
-						lblDiploma.setText("Elevul a ridicat diploma.");
-					}
-					else if ( diplomaAnswer.equals("NU") )
-					{
-						lblDiploma.setText("Elevul nu a ridicat diploma.");
-						
-					}
-					else
-					{
-						lblDiploma.setText("Nu se stie daca are sau nu diploma.");
-					}
- */
